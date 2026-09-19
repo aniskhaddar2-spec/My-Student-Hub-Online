571d18581a97ec148ef39afa9118758a8189983f
aniskhaddar2-spec <aniskhaddar2@gmail.com>
2026-09-19 12:32:16 +0100

    Update student_performance.py

--- files/courses/Data Analysis/Student Performance ML/student_performance.py
+++ files/courses/Data Analysis/Student Performance ML/student_performance.py
@@ -9,7 +9,10 @@ Original file is located at
 
 import pandas as pd
 
-df = pd.read_csv("/StudentPerformanceFactors.csv")
+from pathlib import Path
+
+file_path = Path(__file__).parent / "StudentPerformanceFactors.csv"
+df = pd.read_csv(file_path)
 
 df.head()
 
